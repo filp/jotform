ba378cb69e66a9aabbc6858a417ac2040dee658e
filp <dobreira@gmail.com>
2013-08-23 15:04:58 +0100

    Filter apiKey in VCR cassettes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,11 +4,12 @@ require "webmock/rspec"
 require "vcr"
 require "jotform"
 
-VCR.configure do |c|
-  c.cassette_library_dir = File.join(File.dirname(__FILE__), "fixtures/vcr")
-  c.hook_into :webmock
-end
-
 if ENV["JOTFORM_API_KEY"].nil?
   raise "Missing JotForm API Key, please set ENV[JOTFORM_API_KEY]"
+end
+
+VCR.configure do |c|
+  c.hook_into :webmock
+  c.cassette_library_dir = File.join(File.dirname(__FILE__), "fixtures/vcr")
+  c.filter_sensitive_data("<apiKey>") { ENV["JOTFORM_API_KEY"] }
 end
